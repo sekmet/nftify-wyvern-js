d43c9f22dd36ba42fef377ce79e78333cf725e94
Carlos Guimaraes <sekmet@open4g.com>
2022-04-19 13:55:56 -0300

    first commit

---
+++ lib/utils/deployed.d.ts
@@ -0,0 +1,66 @@
+export declare const deployed: {
+    harmonytestnet: {
+        TestToken: string;
+        TestDAO: string;
+        Migrations: string;
+        MerkleProof: string;
+        WyvernToken: string;
+        WyvernDAO: string;
+        SaleKindInterface: string;
+        WyvernExchange: string;
+        WyvernProxyRegistry: string;
+        TestStatic: string;
+        WyvernDAOProxy: string;
+        WyvernAtomicizer: string;
+        WyvernTokenTransferProxy: string;
+    };
+    rinkeby: {
+        TestToken: string;
+        TestDAO: string;
+        Migrations: string;
+        MerkleProof: string;
+        WyvernToken: string;
+        WyvernDAO: string;
+        WyvernRegistry: string;
+        SaleKindInterface: string;
+        WyvernExchange: string;
+        DirectEscrowProvider: string;
+        WyvernProxyRegistry: string;
+        TestStatic: string;
+        WyvernDAOProxy: string;
+        WyvernAtomicizer: string;
+        WyvernTokenTransferProxy: string;
+    };
+    development: {
+        Migrations: string;
+        TestToken: string;
+        TestDAO: string;
+        MerkleProof: string;
+        WyvernToken: string;
+        WyvernDAO: string;
+        WyvernRegistry: string;
+        SaleKindInterface: string;
+        WyvernExchange: string;
+        DirectEscrowProvider: string;
+        WyvernProxyRegistry: string;
+        TestStatic: string;
+        WyvernDAOProxy: string;
+        WyvernAtomicizer: string;
+        WyvernTokenTransferProxy: string;
+    };
+    main: {
+        Migrations: string;
+        MerkleProof: string;
+        WyvernToken: string;
+        WyvernDAO: string;
+        TestToken: string;
+        TestDAO: string;
+        TestStatic: string;
+        WyvernProxyRegistry: string;
+        SaleKindInterface: string;
+        WyvernExchange: string;
+        WyvernDAOProxy: string;
+        WyvernAtomicizer: string;
+        WyvernTokenTransferProxy: string;
+    };
+};
